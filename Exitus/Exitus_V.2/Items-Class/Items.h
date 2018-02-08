601606c776b9c270c5ebeb0c1706c5948a25e329
dc <35746747+davcruz007@users.noreply.github.com>
2018-02-07 18:28:54 -0800

    Update Items.h

--- Exitus/Exitus_V.2/Items-Class/Items.h
+++ Exitus/Exitus_V.2/Items-Class/Items.h
@@ -1,1 +1,26 @@
 
+#ifndef ITEMS_H
+#define ITEMS_H
+
+
+
+using namespace std;
+
+class Items
+{
+	private:
+		string crowbar();
+		string key();
+		string code();
+
+	public:
+		Items();
+		Items()
+
+};
+
+//does it change?
+
+
+
+#endif
